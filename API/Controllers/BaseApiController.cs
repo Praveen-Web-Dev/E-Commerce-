9e8e2036d6e08d5f02fc1b0ddd64fee9c64ca3a5
Praveen-Web-Dev <praveentej143@yahoo.com>
2025-02-16 00:26:50 +0000

    Upto Pagination API

--- API/Controllers/BaseApiController.cs
+++ API/Controllers/BaseApiController.cs
@@ -7,7 +7,7 @@ using Microsoft.AspNetCore.Mvc;
 namespace API.Controllers
 {
     [Route("api/[controller]")]
-    [ApiController]
+    [ApiController] 
     public class BaseApiController : ControllerBase
     {
      protected async Task<ActionResult> CreatePagedResult<T>(IGenericRepository<T> repo, ISpecification<T> spec, int pageIndex, int pageSize)
